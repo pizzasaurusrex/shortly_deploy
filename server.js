c7f04d50259f30c11cdbd32ad415e4160910942f
MKS-Student <mks.student@hackreactor.com>
2016-10-05 16:42:35 -0700

    Our project is ready

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var app = require('./server-config.js');
 
+
+//will need to change port
 var port = 4568;
 
-app.listen(port);
+app.listen(process.env.PORT || port);
 
 console.log('Server now listening on port ' + port);
+
+console.log('succesful git');
