d059e9d76f0996319cd06e82035ca40fa2bf0e60
MKS-Student <mks.student@hackreactor.com>
2016-10-05 20:08:46 -0700

    Added a comment

--- app/collections/links.js
+++ app/collections/links.js
@@ -1,7 +1,7 @@
 // NOTE: this file is not needed when using MongoDB
 var db = require('../config');
 var Link = require('../models/link');
-
+// a comment
 var Links = new db.Collection();
 
 Links.model = Link;
