7b4a3631810c4a62c0401f996e02e76ad98e6274
MKS-Student <mks.student@hackreactor.com>
2016-10-06 20:00:31 -0700

    Refactor database to Mongo

--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,31 @@
+var mongoose = require('mongoose');
 var db = require('../config');
 var crypto = require('crypto');
 
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  initialize: function() {
-    this.on('creating', function(model, attrs, options) {
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
+var linkSchema = mongoose.Schema({
+    link: String,
+    visits: Number,
+    baseUrl: String,
+    code: String,
+    url: String
 });
 
-module.exports = Link;
+
+var Link = mongoose.model('Link', linkSchema);
+
+var createSha = function(url) {
+      var shasum = crypto.createHash('sha1');
+      shasum.update(url);
+      return shasum.digest('hex').slice(0, 5);
+};  
+
+linkSchema.pre('save', function(next){
+  var code = createSha(this.url);
+  this.code = code;
+  next();
+
+})
+
+
+module.exports = Link; 
+
--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 var app = require('./server-config.js');
 
 
-
 //will need to change port
 var port = 4568;
 
 app.listen(process.env.PORT || port);
 
-console.log('Server now listening on port ' + port);
 
-console.log('succesful git');
 
 
 
